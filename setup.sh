01709bdab8201c1cad2ae5bdd414275d6b76f52e
Dhana <dhana_dhira@apps.ipb.ac.id>
2020-03-03 17:35:08 +0700

    Added setup.sh for automation in installing, please try

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt get update
+
+#Install SSH
+sudo apt-get install ssh -y
+
+#Install Apache dan PHP
+sudo apt install apache2 php mysql-server
+sudo apt install php-mysql php-gd php-mbstring php-xml php-curl
+sudo service apache2 restart
+
+#Clone Repo ADarkRoom
+git clone https://github.com/doublespeakgames/adarkroom.git
+
+#Pindahkan ke /var/www/
+sudo mv ./adarkroom/ /var/www/html/
